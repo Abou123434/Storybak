39c880bde7b18e33175f60c24233d04df31197d8
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-15 23:22:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function renderStories(){
 /* ===== UPLOAD & PREVISUALISATION ===== */
 let previewFile = null; // fichier en cours de prévisualisation
 
-document.getElementById("fileInput").addEventListener("change", e=>{
+document.getElementById("fileInput")("change", e=>{
     let file = e.target.files[0]; 
     if(!file) return;
 
