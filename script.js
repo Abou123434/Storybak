7a11acfc6360789da8d123d03cc00264af3d66c1
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-16 12:05:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -226,7 +226,7 @@ function publishPreviewStory(){
             let videoCount = userStories.filter(s => s.type === "video").length;
 
             if(videoCount + segments > 3){
-                alert(" Maximum 5 vidéo autorisés !");découper automatiquément les vidéos les plus long en segments de 30s
+                alert(" Maximum 5 vidéo autorisés !");
                 return;
             }
 
