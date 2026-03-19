234a38ba78c842515b83d9a1c5e1ea510b302cb1
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-19 22:46:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -676,7 +676,14 @@ if(currentProfile.username === currentUser){
 }
 function nextStory(){ if(currentIndex<users[currentUser].stories.length-1){ currentIndex++; showStory(); } }
 function prevStory(){ if(currentIndex>0){ currentIndex--; showStory(); } }
-function closeViewer(){ clearInterval(timer); document.getElementById("viewer").style.display="none"; }
+function closeViewer(){
+    clearInterval(timer);
+
+    document.getElementById("viewer").style.display = "none";
+
+    // 🔥 remettre le menu
+    document.getElementById("hamburger").style.display = "block";
+}
 
 /* ===== CADEAUX ===== */
 let selectedGiftCost=0, selectedGiftEmoji="";
