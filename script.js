5ce502b46bfea664cea02294d0279a50292f6c03
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-23 20:38:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -413,6 +413,70 @@ function nextStory(){
     }
 }
 
+// ===== STORY =====
+function showStory(){
+    clearInterval(timer);
+
+    let s = users[currentUser].stories[currentIndex];
+
+    renderProgressBars(currentIndex);
+
+    let c = document.getElementById("content");
+    c.innerHTML = "";
+
+    let e;
+
+    // ===== IMAGE =====
+    if(s.type === "image"){
+        e = document.createElement("img");
+        e.src = s.url;
+        c.appendChild(e);
+
+        startProgress(5000);
+    }
+
+    // ===== VIDEO =====
+    else {
+        e = document.createElement("video");
+        e.src = s.url;
+        e.autoplay = true;
+        e.muted = false;
+        e.controls = false;
+
+        c.appendChild(e);
+
+        e.onloadedmetadata = () => {
+            e.currentTime = s.start;
+            e.play();
+        };
+
+        e.ontimeupdate = () => {
+            if(e.currentTime >= s.end){
+                e.pause();
+                nextStory();
+            }
+        };
+
+        startProgress((s.end - s.start) * 1000);
+    }
+
+    // ===== VUES =====
+    if(!s.views[currentProfile.username]){
+        s.views[currentProfile.username] = true;
+        saveData();
+    }
+}
+
+// ===== NEXT =====
+function nextStory(){
+    if(currentIndex < users[currentUser].stories.length - 1){
+        currentIndex++;
+        showStory();
+    } else {
+        closeViewer();
+    }
+}
+
 
 let c=document.getElementById("content"); 
 c.innerHTML="";
