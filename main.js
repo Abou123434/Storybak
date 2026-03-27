187ce33fa1ea57de352d457fa6e761d3d5f0db15
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-27 22:37:58 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+/* ===== HAMBURGER ===== */
+document.getElementById("hamburger").onclick=()=>{
+    let m=document.getElementById("menuOptions");
+    m.style.display=(m.style.display==="flex")?"none":"flex";
+};
