9443fac7bf5a47adcd63f438b226e24554af2a9a
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-30 17:01:23 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 /* ===== HAMBURGER ===== */
 document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("hamburger");
+    const menu = document.getElementById("menuOptions");
 
-  const hamburger = document.getElementById("hamburger");
-  const menu = document.getElementById("menuOptions");
-
-  if(hamburger && menu){
-    hamburger.onclick = () => {
-      menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
-    };
-  }
-
+    btn.addEventListener("click", () => {
+        menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+    });
 });
