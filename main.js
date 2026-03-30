c379ce1830903097ffe424d105081252e25a7b80
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-30 18:45:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 /* ===== HAMBURGER ===== */
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("hamburger");
-    const menu = document.getElementById("menuOptions");
-
-    btn.addEventListener("click", () => {
-        menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
-    });
-});
-
-window.onerror = function(msg, url, line) {
-  alert("ERREUR JS : " + msg + " ligne " + line);
+document.getElementById("hamburger").onclick=()=>{
+    let m=document.getElementById("menuOptions");
+    m.style.display=(m.style.display==="flex")?"none":"flex";
 };
