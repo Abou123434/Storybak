b6ab152a20be4d43bdff0965e798c5685e287f31
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-30 18:02:46 +0000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -357,7 +357,6 @@ if(currentProfile.username === currentUser){
 
     viewer.appendChild(delBtn);
 }
-}
 function nextStory(){ if(currentIndex<users[currentUser].stories.length-1){ currentIndex++; showStory(); } }
 function prevStory(){ if(currentIndex>0){ currentIndex--; showStory(); } }
 function closeViewer(){
