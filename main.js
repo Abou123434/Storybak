c72731fb5b7228147f876dcfee1cf56558bc90b1
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-31 10:46:32 +0000

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("hamburger").onclick = () => {
-    let m = document.getElementById("menuOptions");
-    m.style.display = (m.style.display === "flex") ? "none" : "flex";
-  };
-});
