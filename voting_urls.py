18c65024428908ebabba7411689fa9bcfd9ab640
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-03-31 23:57:43 +0000

    Update voting_urls.py

--- voting_urls.py
+++ voting_urls.py
@@ -1,12 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('voting.urls')),
+    path('', views.vote_page, name='vote_page'),
+    path('vote/<int:image_id>/', views.vote_image, name='vote_image'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
