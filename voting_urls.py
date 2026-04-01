fe67f5740ae77c111424053aa01d2a607d3e5e28
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-04-01 21:16:36 +0000

    Delete voting_urls.py

--- voting_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.vote_page, name='vote_page'),
-    path('vote/<int:image_id>/', views.vote_image, name='vote_image'),
-]
