e09faa104cb657bef99f8a515ddec7aa63bb592a
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-04-23 21:49:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-document.getElementById("resForm").addEventListener("submit", function(e){
-  e.preventDefault();
+function order(item){
+  let phone = "393913404676";
+  let msg = "Ciao, voglio ordinare: " + item;
+  let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(msg);
+  window.open(url, "_blank");
+}
 
-  let name = document.getElementById("name").value;
-  let phone = document.getElementById("phone").value;
-  let date = document.getElementById("date").value;
-  let time = document.getElementById("time").value;
-  let people = document.getElementById("people").value;
-  let message = document.getElementById("message").value;
-
-  let text = `Bonjour, je souhaite réserver une table:%0A
-Nom: ${name}%0A
-Téléphone: ${phone}%0A
-Date: ${date}%0A
-Heure: ${time}%0A
-Personnes: ${people}%0A
-Message: ${message}`;
-
-  let whatsappURL = `https://wa.me/225000000000?text=${text}`;
-
-  window.open(whatsappURL, "_blank");
-});
+function topFunction(){
+  window.scrollTo({top:0, behavior:"smooth"});
+}
