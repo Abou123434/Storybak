75dac1c91d1096e29613dffe937888659771e43f
Abou123434 <jdjdurirjrrj2@gmail.com>
2026-04-23 22:33:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function reserve(plat) {
-  alert("Vous avez choisi : " + plat + " 🍽️");
-}
+// Animation scroll simple (effet pro)
+const sections = document.querySelectorAll("section");
 
-// Formulaire
-document.getElementById("form").addEventListener("submit", function(e) {
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 400;
+    const height = sec.offsetHeight;
+
+    if(top >= offset && top < offset + height){
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
+      sec.style.transition = "0.6s ease";
+    }
+  });
+});
+
+// Formulaire réservation
+document.getElementById("form").addEventListener("submit", function(e){
   e.preventDefault();
-  alert("Réservation envoyée avec succès ✅");
+  alert("Réservation envoyée avec succès 🍽️");
 });
